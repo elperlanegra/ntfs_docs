3bef00bdbda56cdbe8db70e94502a21a98f1cf0d
Diego <diego2000avelar@gmail.com>
2021-10-23 11:09:16 -0600

    create a basic contract

---
+++ scripts/run.js
@@ -0,0 +1,17 @@
+const main = async() =>{
+    const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
+    const gameContract = await gameContractFactory.deploy();
+    await gameContract.deployed();
+    console.log("Contract deployed to:", gameContract.address);
+}
+
+const runMain = async () => {
+  try {
+      await main();
+  } catch (error) {
+      console.log(error);
+      process.exit(1);
+  }
+}
+
+runMain();
